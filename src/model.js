3aca02bef7b053913b8efecdcc1808bb7c31b432
AlionaVashkevych <aliona.vashkevych@gmail.com>
2020-08-26 10:33:32 +0300

    add styles and text

--- src/model.js
+++ src/model.js
@@ -1,17 +1,20 @@
 export const model = [
-    {type: 'title', value: 'Test Title', options: {
+    {type: 'title', value: 'The constructor of sites on pure JS', options: {
       tag: 'h2',
-      styles: 'background: darkred; color: #fff;text-align:center;'
+      styles: 'background: linear-gradient(to left, #ffc7e6, #493240);color: #fff;text-align:center;'
     }},
-    {type: 'text', value: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur error expedit', options: {
-      styles: 'background: darkblue; color: yellow;'
+    {type: 'text', value: 'This site were create by the lessons of <a href="https://youtube.com/c/VladilenMinin" target="_blank">Vladilen Minin</a>.', options: {
+      styles: 'background: linear-gradient(to left, #fefefe, #787878); color: black;text-align:right;height:20px'
     }},
     {type: 'textColumns', value: [
-      '1 text',
-      '2 text',
-      '3 text',
-      '4 text'
-    ], options: {
-      styles: 'padding: 1rem;'
+      'You can use this constructor to create your own site',
+      'Easy to creation your own site with the simple steps',
+      '1 step: choose and add elements; <br>2 step: click the button; <br>3 step: enjoy'
+    ], options: { 
+      styles: 'padding: 2rem 0; color: #fff;background: linear-gradient(to bottom, #deb6f2, #673680);font-weight: bold'
     }},
+    {type: 'image', value: 'some picture', options: {
+      styles: 'padding: 2rem 0;display: flex;justify-content: center;',
+      alt: 'my image'
+    }}
   ]
